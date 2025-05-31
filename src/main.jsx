2fe071fbbe6aa099bb525e752e354962e00df61a
DiAlves96 <145405314+DiAlves96@users.noreply.github.com>
2025-05-31 00:41:18 -0300

    Inicializando projeto React com Vite

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/one-piece-memory-game">
       <App />
     </BrowserRouter>
   </React.StrictMode>
